8178c51ec2fb2c38c5b6fd4df7674cf2e3f46683
Danyal Khuaja <danyal@seed.im>
2022-07-15 15:22:05 +0500

    sdfs

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 console.log("dev firefly ddgit addexchnage not equality not equal dadad")
 fs.writeFile(`file-${(new Date()).toString()}.txt`, "---",function (err) {
     if (err) throw err;
-    console.log('Savasdased!');
+    console.log('Savasdasdsdsdsed!');
   }); 
