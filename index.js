5cad4eee94b87d6d77324222decbf38d39d7e18c
Danyal Khuaja <danyal@seed.im>
2022-07-15 12:00:33 +0500

    sdfs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var fs = require('fs'); 
 console.log("dev firefly exchnage not equality not equal dadad")
-fs.appendFile('mynewfile1.txt', (new Date()).toString()); 
+fs.writeFile(`file-${(new Date()).toString()}.txt`, "---",function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  }); 
