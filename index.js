50a0099d7930bec81322f2e9dcdf284425fcea0f
Danyal Khuaja <danyal@seed.im>
2022-07-15 15:28:33 +0500

    asdad new main

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs'); 
 console.log("dev firefly ddgit addexchnage not equality not equal dadad")
-fs.writeFile(`file-${(new Date()).toString()}.txt`, "---",function (err) {
+fs.writeFile(`file-${(new Date()).toString()}.txt`, "new main ---",function (err) {
     if (err) throw err;
-    console.log('Savasdasdsdsdsed!');
+    console.log('new main Savasdasdsdsdsed!');
   }); 
