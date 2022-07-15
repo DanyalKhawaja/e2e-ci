9ffb3a2f56393f1420e8d0d0a011e9632f4cf1a8
Danyal Khuaja <danyal@seed.im>
2022-07-15 15:53:39 +0500

    with env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 var fs = require('fs'); 
 console.log("dev firefly ddgit addexchnage not equality not equal dadad")
-fs.writeFile(`file-${(new Date()).toString()}.txt`, "new main ---",function (err) {
+fs.writeFile(`file-${(new Date()).toString()}.txt`, process.env.MY_SECRET,function (err) {
     if (err) throw err;
     console.log('new main Savasdasdsdsdsed!');
   }); 
